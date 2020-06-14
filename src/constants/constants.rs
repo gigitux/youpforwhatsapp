6fa81c3f8e20be10981b370a94ecdd5802ccc430
Luigi Teschio <gigitux@gmail.com>
2020-06-14 20:08:52 +0200

    Update naming

--- src/constants/constants.rs
+++ src/constants/constants.rs
@@ -1,5 +1,5 @@
 pub const TITLE: &str = "Whatsapp web";
 pub const URL: &str = "https://web.whatsapp.com";
-pub const APPLICATION_NAME: &str = "org.gigitux.gtkwhats";
+pub const APPLICATION_NAME: &str = "com.gigitux.gtkwhats";
 pub const HEADER_LABEL: &str = "Dark Mode";
 pub const FULL_SCREEN: &str = "Full Screen";
