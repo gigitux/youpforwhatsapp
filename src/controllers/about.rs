52054e35c2b8a9806ce4239436386ab3f9cbacce
gigitux <gigitux@gmail.com>
2020-12-26 15:41:51 +0100

    change url

--- src/controllers/about.rs
+++ src/controllers/about.rs
@@ -5,7 +5,7 @@ pub fn open_about_page() {
     let window = AboutDialog::new();
     window.set_authors(&["<Luigi Teschio> gigitux@gmail.com"]);
     window.set_version(Some("2.0"));
-    window.set_website(Some("https://github.com/gigitux/gtkwhats"));
+    window.set_website(Some("https://luigitesch.io/youpforwhatsapp"));
     let icon = Pixbuf::from_resource("/com/gigitux/youp/logo")
         .ok()
         .and_then(|icon| icon.scale_simple(256, 256, InterpType::Hyper))
